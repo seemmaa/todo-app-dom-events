29794c85e58f2b1387bf2ad9f9ce1e4e744bbed6
Mac <mac@Macs-MacBook-Pro.local>
2025-08-27 17:44:49 +0300

    add local storage

--- script.js
+++ script.js
@@ -5,6 +5,54 @@ var addTaskButton = document.getElementById('addTaskButton');
 var taskInput = document.getElementById('taskInput');
 var taskList = document.querySelector('.taskList');
 
+
+var tasks = localStorage.getItem('tasks');
+if (tasks) {
+    tasks = JSON.parse(tasks);
+    tasks.forEach(function(task) {
+        var listItem = document.createElement('ul');
+        var taskSpan = document.createElement('span');
+        taskSpan.textContent = task.text;
+        if (task.completed) {
+            taskSpan.classList.add('completed');
+        }
+        var deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'delete-button';
+        deleteButton.onclick = function() {
+            taskList.removeChild(listItem);
+            saveTasks();
+        };
+        var checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.checked = task.completed;
+        checkBox.addEventListener('click', function() {
+            taskSpan.classList.toggle('completed');
+            saveTasks();
+        });
+        listItem.appendChild(checkBox);
+        listItem.appendChild(taskSpan);
+        listItem.appendChild(deleteButton);
+        taskList.appendChild(listItem);
+    });
+} else {
+    tasks = [];
+}
+
+function saveTasks() {
+    var taskItems = taskList.querySelectorAll('ul');
+    tasks = [];
+    taskItems.forEach(function(item) {
+        var taskText = item.querySelector('span').textContent;
+        var completed = item.querySelector('input').checked;
+        tasks.push({ text: taskText, completed: completed });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+
+
 function addTask() {
     var taskText = taskInput.value.trim();
     if (taskText === '') {
@@ -22,6 +70,10 @@ function addTask() {
     deleteButton.className = 'delete-button';
     deleteButton.onclick = function() {
         taskList.removeChild(listItem);
+        saveTasks();
+      
+       
+       
     };
 
     var checkBox = document.createElement('input');
@@ -31,11 +83,17 @@ function addTask() {
         taskSpan.classList.toggle('completed');
         
    } );
+  
+
     
     listItem.appendChild(checkBox);
     listItem.appendChild(taskSpan);
     listItem.appendChild(deleteButton);
     taskList.appendChild(listItem);
+    saveTasks();
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+     
     
     taskInput.value = '';//clear
 }
